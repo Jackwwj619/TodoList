c23f60b3cab442deb2b18ae66769319ea5c4519f
Jack <jackwwj619@gmail.com>
2024-06-03 21:19:04 +0800

    成功运行，但是页面交互存在问题，只能显示单薄的html页面，而一些静态资源文件并未派上用场，业务逻辑实现较差

--- src/main/java/com/todo/controller/TaskController.java
+++ src/main/java/com/todo/controller/TaskController.java
@@ -1,14 +1,10 @@
 package com.todo.controller;
 
-
 import com.todo.service.TaskService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.time.LocalDate;
@@ -18,35 +14,36 @@ import java.util.Map;
 public class TaskController {
 
     @Autowired
-    TaskService taskService;
+    private TaskService taskService;
 
     @ExceptionHandler(RuntimeException.class)
-    public ModelAndView handleUnkonwException(Exception ex) {
+    public ModelAndView handleUnknownException(Exception ex) {
         return new ModelAndView("500.html", Map.of("error", ex.getClass().getSimpleName(), "message", ex.getMessage()));
     }
 
-
-    public ModelAndView index(HttpSession session) {
+    @GetMapping("/task")
+    public ModelAndView index() {
         Map<String, Object> model = Map.of("tasks", taskService.getAllTasks());
         return new ModelAndView("task.html", model);
     }
+//    public String test() {
+//        return "Hello World";
+//    }
 
     @PostMapping("/add")
-    public ModelAndView addTask(@RequestParam("title")String title, @RequestParam("description")String description, @RequestParam("dueDate") LocalDate dueDate) {
-        try {
-            taskService.addTask(title, description, dueDate);
-        } catch (RuntimeException e) {
-            return new ModelAndView("task.html", Map.of("error", "Add task failed."));
-        }
+    public ModelAndView addTask(@RequestParam("title") String title,
+                                @RequestParam("description") String description,
+                                @RequestParam("dueDate") LocalDate dueDate) {
+        taskService.addTask(title, description, dueDate);
         return new ModelAndView("redirect:/task");
     }
 
-    @GetMapping("calender")
+    @GetMapping("/calender")
     public ModelAndView calender() {
         return new ModelAndView("calender.html");
     }
 
-    @GetMapping("stati")
+    @GetMapping("/stati")
     public ModelAndView stati() {
         return new ModelAndView("stati.html");
     }
